0cc928e95b44fb7ae052d35d059fc0530a2949bb
Joe Pinsonault <joe.pinsonanult@puppetlabs.com>
2014-09-07 19:21:52 -0700

    Removed extra stuff from parser file

--- clausewitz_parser.py
+++ clausewitz_parser.py
@@ -12,8 +12,6 @@ import argparse
 import re
 from pprint import pprint
 
-greet = Word( alphas ) + Literal(",").suppress() + Word( alphas ) + Literal("!").suppress()
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -27,9 +25,6 @@ def parse_args():
 def main(args):
     with open(args.save_file, 'r') as save_file:
         save_file_string = save_file.read()
-
-    greeting = greet.parseString( "Hello, World!" )
-    print greeting
 
 
 if __name__ == '__main__':
